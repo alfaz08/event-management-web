ad15abca483b364154066a37cc177da7d0f44d99
Alfaz Hossain <alfaz.cse@gmail.com>
2023-10-07 11:50:21 +0600

    first commit

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/pages/Registration/Registration.jsx
@@ -0,0 +1,11 @@
+
+
+const Registration = () => {
+  return (
+    <div>
+      <h2>This is registration</h2>
+    </div>
+  );
+};
+
+export default Registration;
